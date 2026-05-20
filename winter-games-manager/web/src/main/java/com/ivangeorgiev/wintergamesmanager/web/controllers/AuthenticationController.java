7890fe6993febbce4e26e85f840341aea146d1cd
Ivan Georgiev <ivan.georgiev0904@gmail.com>
2026-05-20 19:01:50 +0300

    Add auth controller

---
+++ winter-games-manager/web/src/main/java/com/ivangeorgiev/wintergamesmanager/web/controllers/AuthenticationController.java
@@ -0,0 +1,18 @@
+package com.ivangeorgiev.wintergamesmanager.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller()
+public class AuthenticationController {
+
+    @GetMapping("login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("register")
+    public String register(){
+        return "register";
+    }
+}
